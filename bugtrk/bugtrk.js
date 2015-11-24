0194c59f4a9c7231b49bb251c599ece9c5d6ecf7
Anton <anton.yacenko@gmail.com>
2015-11-24 02:29:16 +0200

    bugtrk

---
+++ bugtrk/bugtrk.js
@@ -0,0 +1,50 @@
+#!/usr/local/bin/node
+
+// var questions = [
+//   [10, 2, 3, 9],
+//   [2, 1000000000, 999999999, 1999999998],
+//   [4, 1, 1, 2],
+// ];
+//
+// questions.forEach(function(q) {
+//   console.log(run(q));
+// });
+var fs = require('fs');
+var data = fs.readFileSync('bugtrk.in', 'utf-8');
+var q = data.split('\n')[0].split(' ');
+
+var r = run(q);
+// console.log(r);
+fs.writeFileSync('bugtrk.out', r);
+
+function run(q) {
+  var N = q[0] * 1;
+  var W = q[1] * 1;
+  var H = q[2] * 1;
+
+  if (W > H) {
+    H = q[1] * 1;
+    W = q[2] * 1;
+  }
+
+  var S = N * W * H;
+  var root = Math.sqrt(S);
+
+  var cols = Math.ceil(root / W);
+  var newS = Math.pow(cols * W, 2);
+
+  var newRoot = Math.sqrt(newS);
+
+  //check if we can fit all
+  var rows = Math.floor(newRoot / H);
+
+  var newN = cols * rows;
+
+  //we can't fit all, so adding 1 row
+  if (newN < N) {
+    rows++;
+    newRoot = rows * H;
+  }
+
+  return newRoot;
+}
--- hamstr/hamstr.js
+++ hamstr/hamstr.js
@@ -3,7 +3,7 @@
 // console.time('op')
 
 var fs = require('fs');
-var data = fs.readFileSync('hamstr.in', 'utf-8').split("\n");
+var data = fs.readFileSync('hamstr.in', 'utf-8').split('\n');
 // var data = fs.readFileSync('08.in.txt', 'utf-8').split("\n");
 // var data = fs.readFileSync('t/11.in', 'utf-8').split("\n");
 
@@ -11,11 +11,10 @@ var data = fs.readFileSync('hamstr.in', 'utf-8').split("\n");
 
 // console.log('file read')
 
-RUN(data)
-
+RUN(data);
 
 var dir = 't/';
-var t = ['01.in', '02.in', '03.in', '04.in', '05.in', '06.in', '07.in', '08.in', '09.in', '10.in', '11.in', '12.in']
+var t = ['01.in', '02.in', '03.in', '04.in', '05.in', '06.in', '07.in', '08.in', '09.in', '10.in', '11.in', '12.in'];
 
 // t.forEach(function(tt){
 //   var d = fs.readFileSync(dir+tt, 'utf-8').split("\n");
@@ -25,15 +24,15 @@ var t = ['01.in', '02.in', '03.in', '04.in', '05.in', '06.in', '07.in', '08.in',
 //   console.log('')
 // })
 
-function RUN(data){
+function RUN(data) {
   var memoisedMap = {};
 
-  var S = data[0]*1;
-  var C = data[1]*1;
+  var S = data[0] * 1;
+  var C = data[1] * 1;
   var HC = [];
-  for(var i = 0; i<C; i++){
-  	var t = data[i+2].split(' ');
-  	HC.push([t[0]*1, t[1]*1]);
+  for (var i = 0; i < C; i++) {
+    var t = data[i + 2].split(' ');
+    HC.push([t[0] * 1, t[1] * 1]);
   }
 
   // console.log('HC array', HC.length)
@@ -58,7 +57,6 @@ function RUN(data){
   //
   // RESULT 2
 
-
   // TEST 2
   //
   // var S = 19;
@@ -72,7 +70,6 @@ function RUN(data){
 
   // RESULT 3
 
-
   // TEST 3
   //
   // var S = 2;
@@ -84,36 +81,33 @@ function RUN(data){
   //
   // RESULT 1
 
-
   // TODO
   // more smart FOR :)
 
-
-
-  if( C > 99999 ){
+  if (C > 99999) {
     var start = 0;
     var end = C;
 
-    function middle(){
-      return Math.ceil(start + (end - start)/2);
+    function middle() {
+      return Math.ceil(start + (end - start) / 2);
     }
 
     var lastI;
 
-    function next(){
+    function next() {
       var i = middle();
-      if( i == lastI ){
-        if( !RESULT) {
-          if( proceed(0) <= S ){
+      if (i == lastI) {
+        if (!RESULT) {
+          if (proceed(0) <= S) {
             RESULT = 1;
           }
         }
         return;
       }
       lastI = i;
-      var t = proceed(i)
+      var t = proceed(i);
       // console.log(start, end, i, lastI, t, S)
-      if( t <= S ){
+      if (t <= S) {
         RESULT = i;
         start = i;
         // console.log('INCREASE')
@@ -126,32 +120,31 @@ function RUN(data){
     }
 
     next();
-  }
-  else{
-    for( var i = C; i>0; i--){
-      t = proceed(i - 1)
-      if( t <= S ){
+  } else {
+    for (var i = C; i > 0; i--) {
+      t = proceed(i - 1);
+      if (t <= S) {
         RESULT = i;
         i = 0;
       }
     }
   }
 
-  if( RESULT ) {
+  if (RESULT) {
     //check this corner case, i have no idea why is that
-    var max = HC[0][0]
-    for(var i = 1; i<HC.length; i++){
-      if( HC[i][0] > max ) {
+    var max = HC[0][0];
+    for (var i = 1; i < HC.length; i++) {
+      if (HC[i][0] > max) {
         max = HC[i][0];
       }
     }
-    var maxH = HC[0][1]
-    for(var i = 1; i<HC.length; i++){
-      if( HC[i][1] > maxH ) {
+    var maxH = HC[0][1];
+    for (var i = 1; i < HC.length; i++) {
+      if (HC[i][1] > maxH) {
         maxH = HC[i][1];
       }
     }
-    if( max == 0 && maxH > 0){
+    if (max == 0 && maxH > 0) {
       RESULT++;
     }
   }
@@ -160,12 +153,12 @@ function RUN(data){
   // console.log(RESULT || 0);
   fs.writeFileSync('hamstr.out', RESULT || 0);
 
-  function proceed(count){
-    if( count == 0){
+  function proceed(count) {
+    if (count == 0) {
       // console.log('HERE')
-      var min = HC[0][0]
-      for(var i = 1; i<HC.length; i++){
-        if( HC[i][0] < min ) {
+      var min = HC[0][0];
+      for (var i = 1; i < HC.length; i++) {
+        if (HC[i][0] < min) {
           min = HC[i][0];
         }
       }
@@ -178,7 +171,7 @@ function RUN(data){
     var all = totalsForAllHamsters(count);
     // console.log('totals', all)
     var t = 0;
-    for(var j = 0; j<count; j++){
+    for (var j = 0; j < count; j++) {
       t += all[1][j];
     }
     // console.log(t, ' is minimum for ', j, ' of ',S)
@@ -186,77 +179,76 @@ function RUN(data){
     return t;
   }
 
-  function total(arr){
-  	var t = 0;
-  	arr.forEach(function(item){
-  		t += item;
-  	});
-  	return t;
+  function total(arr) {
+    var t = 0;
+    arr.forEach(function(item) {
+      t += item;
+    });
+    return t;
   }
 
-  function totalsForAllHamsters(newC){
-  	var r = [];
-  	for(var i=0; i<C; i++){
-  		r.push(totalForOneHamster(i, newC));
-  	}
-  	return [newC, merge_sort(r)];
+  function totalsForAllHamsters(newC) {
+    var r = [];
+    for (var i = 0; i < C; i++) {
+      r.push(totalForOneHamster(i, newC));
+    }
+    return [newC, merge_sort(r)];
   }
 
-  function totalForOneHamster(i, C){
+  function totalForOneHamster(i, C) {
     var a = HC[i][0];
     var b = HC[i][1];
-  	return memoised(a,b,C);
+    return memoised(a,b,C);
   }
 
-  function memoised(a,b,c){
-    return memoisedMap[a+'_'+b+'_'+c] || addmemoise(a,b,c);
-    function addmemoise(a,b,c){
+  function memoised(a, b, c) {
+    return memoisedMap[a + '_' + b + '_' + c] || addmemoise(a,b,c);
+    function addmemoise(a, b, c) {
       // console.log('')
       // console.log('MEMOISE!!!!!')
       // console.log('')
-      memoisedMap[a+'_'+b+'_'+c] = a + b*c;
-      return memoisedMap[a+'_'+b+'_'+c];
+      memoisedMap[a + '_' + b + '_' + c] = a + b * c;
+      return memoisedMap[a + '_' + b + '_' + c];
     }
   }
 
   //Merge sort
-  function isLess(a,b){
-  	return a*1 < b*1;
+  function isLess(a, b) {
+    return a * 1 < b * 1;
   }
-  function merge_sort(arr){
+  function merge_sort(arr) {
 
-    return mergesort_recursive(arr, 0, arr.length-1);
+    return mergesort_recursive(arr, 0, arr.length - 1);
 
     function mergesort_recursive(array, start, end) {
-      if( start >= end ) {
-        return [array[start]]
+      if (start >= end) {
+        return [array[start]];
       }
-      var middle = Math.floor((start + end) / 2)
+      var middle = Math.floor((start + end) / 2);
       var left_part = mergesort_recursive(array, start, middle);
       var right_part = mergesort_recursive(array, middle + 1, end);
       return merge(left_part, right_part);
     }
 
-
-    function merge(A,B){
+    function merge(A, B) {
       var aPointer = 0;
       var bPointer = 0;
       var writePointer = 0;
       var R = [];
 
-      while( aPointer < A.length && bPointer < B.length ) {
-        if( isLess(A[aPointer], B[bPointer]) ){
+      while (aPointer < A.length && bPointer < B.length) {
+        if (isLess(A[aPointer], B[bPointer])) {
           R[writePointer++] = A[aPointer++];
         } else {
           R[writePointer++] = B[bPointer++];
         }
       }
 
-      while( aPointer < A.length ) {
+      while (aPointer < A.length) {
         R[writePointer++] = A[aPointer++];
       }
 
-      while( bPointer < B.length ) {
+      while (bPointer < B.length) {
         R[writePointer++] = B[bPointer++];
       }
 
